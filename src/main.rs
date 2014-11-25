c7e735d83fe42ef407b4cf206b2ecc305900a23a
bryal <96.bryal@gmail.com>
2014-11-25 22:47:51 +0100

    Changed license to GPLv3
    Changed license and added copyright notices in source files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-#![feature(globs)]
+// Copyright (C) 2014  Johan Johansson
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 extern crate current;
 extern crate shader_version;
 extern crate input;
